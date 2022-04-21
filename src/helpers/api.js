dc446be3c33be003fbe576c34d8cf4fa375a3d5e
gabriel <gabriel@localhost.localdomain>
2022-04-21 18:01:25 -0400

    tree commit

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,2 +1,3 @@
-export const api = "http://192.168.0.100:4000/api";
+/*export const api = "http://192.168.0.100:4000/api";*/
+export const api = "https://beggubo.com/services/api";
 export const local = "http://localhost:3000";
